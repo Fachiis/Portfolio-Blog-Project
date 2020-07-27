9736e4ce40ba1d24c224dba97698b6893aa7d78a
Fachiis <zashafachii@gmail.com>
2020-07-27 03:59:56 +0100

    Modified settings

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0)) #Production
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost'] #Production
+
+ALLOWED_HOSTS = [] 
+#ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost'] #Production
 
 
 # Application definition
